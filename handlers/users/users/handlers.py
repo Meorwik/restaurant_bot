ee2a9371be599b692c03484a37309aa695d3d137
Meorwik <104008026+Meorwik@users.noreply.github.com>
2023-12-04 05:14:42 +0600

    bug fix again

--- handlers/users/users/handlers.py
+++ handlers/users/users/handlers.py
@@ -254,7 +254,8 @@ async def handle_quantity_selection_menu(call: types.CallbackQuery, state: FSMCo
 
         quantity_selection_menu: QuantitySelectionMenu = data["quantity_selection_menu"]
         current_product: Product = data["current_product"]
-        quantity_selection_status = data["quantity_selection_status"]
+        if "quantity_selection_status" in data:
+            quantity_selection_status = data["quantity_selection_status"]
 
     plus_one_callback = quantity_selection_menu.plus_one_callback
     minus_one_callback = quantity_selection_menu.minus_one_callback
